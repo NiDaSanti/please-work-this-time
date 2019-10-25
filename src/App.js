891e5f38b1b5c78832deb07674cf942adf73bfe0
nasantiago <santiago.nicholas.a@gmail.clmgit config --global user.email santiago.nicholas.a@gmail.com>
2019-10-24 22:12:28 -0400

    uptaded getFiveDayForcast setState

--- src/App.js
+++ src/App.js
@@ -26,70 +26,75 @@ class App extends React.Component {
   getFiveDayForecast = async (zipCode) => {
     const second_api_call = await fetch(`https://api.openweathermap.org/data/2.5/forecast?zip=${zipCode},us&appid=${NEW_API_KEY}`)
     const newData = await second_api_call.json();
-    this.setState = {
-      currentDate: newData.list.dt,
-      temperature: newData.list.main.temp.temp_min.temp_max,
-      windSpeed: newData.list.wind.speed,
-      humidity: newData.list.main.humidity,
-      descripion: newData.list.weather.descripion[0]
+    console.log(newData);
+      this.setState = {
+        currentDate: newData.list.dt,
+        temperature: newData.list.main.temp.temp_min.temp_max,
+        windSpeed: newData.list.wind.speed,
+        humidity: newData.list.main.humidity,
+        descripion: newData.list.weather[0].descripion
+      } 
+      
+      /*const DailyCard = ({ reading }) => {
+      let newDate = new Date();
+      const weekday = reading.dt * 1000
+      newDate.setTime(weekday)
+      moment(newDate).format('dddd')
+      moment(newDate).format('MMMM Do, h:mm a')*/
+      /*console.log(newData);
+      let daysOfTheWeek = [];
+      for(let i = 0; i < 5; i++) {
+        //console.log('yo',newData.list[i])
+        daysOfTheWeek.push(newData.list[i])
+        //})
+      }*/
+      //console.log('final array', daysOfTheWeek);
+    }
+        getWeather = async (zipCode) => {
+        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${API_KEY}`)
+        const data = await api_call.json();
+        const kTemp = data.main.temp;
+        const fTemp = kTemp * (9 / 5) - 459.67;
+        
+        if(zipCode) {                   // if true, then display the setState, 
+         //console.log(data);
+         this.setState({
+         temperature: fTemp.toFixed(0),
+         city: data.name,
+         country: data.sys.country,
+         humidity: data.main.humidity,
+         description: data.weather[0].description,
+         airPressure: data.main.pressure,
+         error: ""
+        })
+      } else {
+        this.setState({               // else, display error message
+          error: "Enter a zip code."
+        })
+      }
+    }
+        
+      render() {
+       return (
+        <div className="App">
+          <FormData getWeather={this.getWeather}
+            getZipCode={this.getZipCode}
+            getFiveDayForecast={this.getFiveDayForecast}
+          />
+          <Weather/>
+          <WeatherDisplay
+            temperature={this.state.temperature}
+            city={this.state.city}
+            country={this.state.country}
+            humidity={this.state.humidity}
+            description={this.state.description}
+            airPressure={this.state.pressure}
+            error={this.state.error}
+          />
+        </div>
+      );
+     }
     }
     
-    /*const DailyCard = ({ reading }) => {
-    let newDate = new Date();
-    const weekday = reading.dt * 1000
-    newDate.setTime(weekday)
-    moment(newDate).format('dddd')
-    moment(newDate).format('MMMM Do, h:mm a')*/
-    /*console.log(newData);
-    let daysOfTheWeek = [];
-    for(let i = 0; i < 5; i++) {
-      //console.log('yo',newData.list[i])
-      daysOfTheWeek.push(newData.list[i])
-      //})
-    }*/
-    //console.log('final array', daysOfTheWeek);
-}
-    getWeather = async (zipCode) => {
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${API_KEY}`)
-    const data = await api_call.json();
-    const kTemp = data.main.temp;
-    const fTemp = kTemp * (9 / 5) - 459.67;
-   
-     if(zipCode) {                   // if true, then display the setState, 
-      //console.log(data);
-      this.setState({
-      temperature: fTemp.toFixed(0),
-      city: data.name,
-      country: data.sys.country,
-      humidity: data.main.humidity,
-      description: data.weather[0].description,
-      airPressure: data.main.pressure,
-      error: ""
-    })
-    } else {
-      this.setState({               // else, display error message
-        error: "Enter a zip code."
-    })
-  }
-}
-  render() {
-   return (
-    <div className="App">
-      <FormData getWeather={this.getWeather}
-      getZipCode={this.getZipCode}/>
-      <Weather/>
-      <WeatherDisplay
-        temperature={this.state.temperature}
-        city={this.state.city}
-        country={this.state.country}
-        humidity={this.state.humidity}
-        description={this.state.description}
-        airPressure={this.state.pressure}
-        error={this.state.error}
-      />
-    </div>
-  );
- }
-}
-
-export default App;
+    export default App;
+    
