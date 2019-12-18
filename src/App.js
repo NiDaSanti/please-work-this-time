b060767fb13c749c315f6d1646de1378d03db589
nasantiago <santiago.nicholas.a@gmail.clmgit config --global user.email santiago.nicholas.a@gmail.com>
2019-12-18 11:49:30 -0500

    finally updated //// - - \\ Images/fiveday forecast

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import FormData from './components/FormData'
 import './App.css';
 import WeatherDisplay from './components/WeatherDisplay';
-import FiveDayForecast from './components/FiveDayForecast'
+ import DayForecast from './components/FiveDayForecast'
+//import Day from './components/Day'
 import moment from 'moment';
 import thunderStorm from './images/bolt-solid.svg'
 import drizzle from './images/cloud-rain-solid.svg'
@@ -12,11 +13,17 @@ import atmosphere from './images/smog-solid.svg'
 import sunny from './images/sun-regular.svg'
 import cloudy from './images/cloud-solid.svg'
 
+
+//import SvGIcons from './components/SvGIcons'
+
 const API_KEY = "8344d85e6a5e4fef120ba16a34295611";
 const NEW_API_KEY = "8c57dcc6ff663280af8e253b10826c32";
 
 class App extends React.Component {
-  state = { newList: [] }
+  state = {
+    forecast: false
+  }
+
   
   kelvinToFarenheit(k) {
     let kelvin = k - 273.15;
@@ -32,154 +39,105 @@ class App extends React.Component {
     this.getWeather(zipCode)
     this.getFiveDayForecast(zipCode)
   }
-
-   getWeather = async (zipCode) => {
+  
+  getCurrentWeatherIcon = (data) => {
+    console.log('weather condition', data);
+    if(data.weather[0].id >= 200 && data.weather[0].id <= 232) {
+      return thunderStorm
+    }
+    if(data.weather[0].id >= 300 && data.weather[0].id <= 321) {
+      return drizzle
+    }
+    if(data.weather[0].id >= 500 && data.weather[0].id <= 531) {
+      return rain
+    }
+    if(data.weather[0].id >= 600 && data.weather[0].id <= 622) {
+      return snow
+    }
+    if(data.weather[0].id >= 701 && data.weather[0].id <= 781) {
+      return atmosphere
+    }
+    if(data.weather[0].id === 800) {
+      return sunny
+    }
+    if(data.weather[0].id >= 801 && data.weather[0].id <= 804) {
+      console.log('cloudy',cloudy)
+      return cloudy
+    } 
+  }
+  
+  getWeather = async (zipCode) => {
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${API_KEY}`)
     const data = await api_call.json();
     const kTemp = data.main.temp;
     const fTemp = kTemp * (9 / 5) - 459.67;
     console.log('hello everyone', data)
-
-    let SvgIcons = {
-      thunderStorm,
-      drizzle,
-      rain,
-      snow,
-      atmosphere,
-      sunny,
-      cloudy
-    }
-   
-    this.getCurrentWeatherIcon = () => {
-      if(data.weather[0].id >= 200 && data.weather[0].id <= 232) {
-        return thunderStorm
-      }
-      if(data.weather[0].id >= 300 & data.weather[0].id <= 321) {
-        return drizzle
-      }
-      if(data.weather[0].id >= 500 && data.weather[0].id <= 531) {
-        return rain
-      }
-      if(data.weather[0].id >= 600 && data.weather[0].id <= 622) {
-        return snow
-      }
-      if(data.weather[0].id >= 701 && data.weather[0].id <= 781) {
-        return atmosphere
-      }
-      if(data.weather[0].id === 800) {
-        return sunny
-      }
-      if(data.weather[0].id >= 801 && data.weather[0].id <= 804) {
-        return cloudy
-      } else {
-        return "Cannot read"
-      }
-      
-    }
-   // console.log('Today is the', this.getCurrentWeatherIcon(SvgIcons))
     
-    if(zipCode) {                   // if true, then display the setState, 
-     this.setState({
-     temperature: fTemp.toFixed(0),
-     icon: this.getCurrentWeatherIcon(SvgIcons),
-     city: data.name,
-     country: data.sys.country,
-     humidity: data.main.humidity,
-     description: data.weather[0].description,
-     airPressure: data.main.pressure,
-     
-     error: ""
-    })
-    console.log(data);
-  } else {
-    this.setState({               // else, display error message
-      error: "Enter a zip code."
-    })
+    
+    if(zipCode) {        
+      console.log()           
+      this.setState({
+        temperature: fTemp.toFixed(0),
+        icon: this.getCurrentWeatherIcon(data),
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        airPressure: data.main.pressure,
+        
+        error: ""
+      }, () => {console.log(this.state.icon);})
+      
+    } else {
+      this.setState({               
+        error: "Enter a zip code."
+      })
+    }
   }
-}
+  
+  getFiveDayForecast = async (zipCode) => {
+    const second_api_call = await fetch(`https://api.openweathermap.org/data/2.5/forecast?zip=${zipCode},us&appid=${NEW_API_KEY}`)
+    const newData = await second_api_call.json();
+    
+    let today = [];
+    let day2 = [];
+    let day3 = [];
+    let day4 = [];
+    let day5 = [];
+
+    console.log('what is', today)
+    await newData.list.map ((i) => {
+      
+      if (moment(i.dt_txt).format('MM DD') === moment().format('MM DD')) {
+        today.push(i)
+
+      }
+      if (moment(i.dt_txt).format('MM DD') === moment().add(1, 'Days').format('MM DD')) {
+        day2.push(i) 
+      }
+      if (moment(i.dt_txt).format('MM DD') === moment().add(2, 'Days').format('MM DD')) {
+        day3.push(i)
+      }
+      if (moment(i.dt_txt).format('MM DD') === moment().add(3, 'Days').format('MM DD')) {
+        day4.push(i)
+      }
+      if (moment(i.dt_txt).format('MM DD') === moment().add(4, 'Days').format('MM DD')) {
+        day5.push(i)
+      }
+      return i
+    })
+
+this.setState({
+  today: today,
+  day2: day2,
+  day3: day3,
+  day4: day4,
+  day5: day5,
+  })
  
-getFiveDayForecast = async (zipCode) => {
-  const second_api_call = await fetch(`https://api.openweathermap.org/data/2.5/forecast?zip=${zipCode},us&appid=${NEW_API_KEY}`)
-  const newData = await second_api_call.json();
 
-  let SvgIcons2 = {
-    thunderStorm,
-    drizzle,
-    rain,
-    snow,
-    atmosphere,
-    sunny,
-    cloudy
-  }
-
-  let today = [];
-  let day2 = [];
-  let day3 = [];
-  let day4 = [];
-  let day5 = [];
-
-  newData.list.map = ((i) => {
-    console.log(i)
-if (moment(i.dt_txt).format('MM DD') === moment().format('MM DD')) {
- today.push(i)
 }
-if (moment(i.dt_txt).format('MM DD') === moment().add(1, 'Days').format('MM DD')) {
- day2.push(i)
-}
-if (moment(i.dt_txt).format('MM DD') === moment().add(2, 'Days').format('MM DD')) {
-  day3.push(i)
-}
-if (moment(i.dt_txt).format('MM DD') === moment().add(3, 'Days').format('MM DD')) {
-  day4.push(i)
-}
-if (moment(i.dt_txt).format('MM DD') === moment().add(4, 'Days').format('MM DD')) {
-  day5.push(i)
-}
-
-    // if (i.dt_txt === today + 2) {
-    //   return day3.push(i)
-    // }
-    // if (i.dt_txt === today + 3) {
-    //   return day4.push(i)
-    // }
-    // if (i.dt_txt === today + 4) {
-    //   return day5.push(i)
-    // }
-
-  })
-
-  this.getMinTemp = (array, max) => {
-    max = 0;
-    array.map = (item => {
-      if(item.main.temp_max > max) {
-        max = item.main.temp_max;
-      }
-    })
-    return max
-  }
-
-  this.getMinTemp = (array, min) => {
-    min = 400
-    array.map = (item => {
-      if(item.main.temp_max < min) {
-        min = item.main.temp_min;
-      }
-    })
-    return min
-  }
-
-  this.setState ({
-    today: this.state.today.dt_txt,
-    day2: this.state.day2.dt_txt,
-    day3: this.state.day3.dt_txt,
-    day4: this.state.day4.dt_txt,
-    day5: this.state.day5.dt_txt
-  })
-  console.log('today',)
-}
-
-  render() {
-    //console.log("HELLO", this.state.newList[0])
+render() {
     return (
     <div className="App">
       <FormData getWeather={this.getWeather}
@@ -187,7 +145,7 @@ if (moment(i.dt_txt).format('MM DD') === moment().add(4, 'Days').format('MM DD')
         getFiveDayForecast={this.getFiveDayForecast}
       />
 
-      <WeatherDisplay
+      {this.state.temperature && <WeatherDisplay
         temperature={this.state.temperature}
         icon={this.state.icon}
         currentWeatherIcon={this.state.currentWeatherIcon}
@@ -197,53 +155,39 @@ if (moment(i.dt_txt).format('MM DD') === moment().add(4, 'Days').format('MM DD')
         description={this.state.description}
         airPressure={this.state.pressure}
         error={this.state.error} 
-        />
+        /> }
         
-        <FiveDayForecast
-        
-        today={this.state.today}
-        icon={this.state.icon}
-        windSpeed={this.state.windSpeed}
-        currentTemperature={this.state.currentTemperature && this.kelvinToFarenheit(this.state.currentTemperature)}
-        overallDescription={this.state.overallDescription}
-        overallHumidity={this.state.overallHumidity}
-        weatherIcon={this.state.weatherIcon}
-
-        day2={this.state.day2}
-        icon2={this.state.icon2}
-        windSpeed2={this.state.windSpeed2}
-        overallTemperature2={this.state.overallTemperature2 && this.kelvinToFarenheit(this.state.overallTemperature2)}
-        overallDescription2={this.state.overallDescription2}
-        overallHumidity2={this.state.overallHumidity2}
-        weatherIcon2={this.state.weatherIcon}
-
-        day3={this.state.day3}
-        icon3={this.state.icon3}
-        windSpeed3={this.state.windSpeed3}
-        overallTemperature3={this.state.overallTemperature3 && this.kelvinToFarenheit(this.state.overallTemperature3)}
-        overallDescription3={this.state.overallDescription3}
-        overallHumidity3={this.state.overallHumidity3}
-
-        day4={this.state.day4}
-        icon4={this.state.icon4}
-        windSpeed4={this.state.windSpeed4}
-        overallTemperature4={this.state.overallTemperature4 && this.kelvinToFarenheit(this.state.overallTemperature4)}
-        overallDescription4={this.state.overallDescription4}
-        overallHumidity4={this.state.overallHumidity4}
-
-        day5={this.state.day5}
-        icon5={this.state.icon5}
-        windSpeed5={this.state.windSpeed5}
-        overallTemperature5={this.state.overallTemperature5 && this.kelvinToFarenheit(this.state.overallTemperature5)}
-        overallDescription5={this.state.overallDescription5}
-        overallHumidity5={this.state.overallHumidity5} 
+      {this.state.today && 
+      <ul>
+      <DayForecast
+          weather={this.state.today}
       />
-    </div>
+       <DayForecast
+          weather={this.state.day2}
+      />
+       <DayForecast
+          weather={this.state.day3}
+      />
+       <DayForecast
+          weather={this.state.day4}
+      />
+       <DayForecast
+          weather={this.state.day5}
+      />
+      </ul> }
+      </div> 
+    
+  
+
+     
+   
+
   );
  }
 }
 
 export default App;
+
     
     
                   
--- src/components/FiveDayForecast/index.js
+++ src/components/FiveDayForecast/index.js
@@ -1,49 +1,118 @@
 import React from 'react'
+// import thunderStorm from '../images/bolt-solid.svg'
+// import drizzle from './images/cloud-rain-solid.svg'
+// import rain from './images/cloud-showers-heavy-solid.svg'
+// import snow from './images/snowflake-regular.svg'
+// import atmosphere from './images/smog-solid.svg'
+import sunny from '../../images/sun-regular.svg'
+// import cloudy from './images/cloud-solid.svg'
 
 
-const FiveDayForecast = (props) => (
-    <div className = "five-day-container">
-        <div className = "first-day-container">
-            {props.currentDate && <p className = "current-time">{props.currentDate}</p>}
-            <img src = {props.icon} alt = 'conditions'></img>
-            {props.currentTemperature && <p className = "current-temp">{props.currentTemperature}</p>}
-            {props.windSpeed && <p className = "wind">{props.windSpeed}</p>} 
-            {props.overallHumidity && <p className = "get-humid"> {props.overallHumidity}</p>}
-            {props.overallDescription && <p className = "conditions">Conditions: {props.overallDescription}</p>}
-        </div>
-        <div className = "second-day-container">
-            {props.day2 && <p className = "current-time">{props.day2}</p>}
-            <img className = 'style-condition' src = {props.icon2} alt = 'conditions'></img>
-            {props.overallTemperature2 && <p className = "current-temp">{props.overallTemperature2}</p>}
-            {props.windSpeed2 && <p className = "wind">{props.windSpeed2}</p>}
-            {props.overallHumidity2 && <p className = "get-humid">{props.overallHumidity2}</p>}
-            {props.overallDescription2 && <p className = "conditions">Conditions: {props.overallDescription2}</p>}
-        </div>
-        <div className = "third-day-container">
-            {props.day3 && <p className = "current-time">{props.day3}</p>}
-            <img className = 'style-condition' src = {props.icon3} alt = 'rain and clouds'></img>
-            {props.overallTemperature3 && <p className = "current-temp">{props.overallTemperature3}</p>}
-            {props.windSpeed3 && <p className = "wind">{props.windSpeed3}</p>}
-            {props.overallHumidity3 && <p className = "get-humid">{props.overallHumidity3}</p>}
-            {props.overallDescription3 && <p className = "conditions">Conditions: {props.overallDescription3}</p>}
-        </div>
-        <div className = "fourth-day-container">
-            {props.day4 && <p className = "current-time">{props.day4}</p>}
-            <img className = 'style-condition' src = {props.icon4} alt = 'rain and clouds'></img>
-            {props.overallTemperature4 && <p className = "current-temp">{props.overallTemperature4}</p>}
-            {props.windSpeed4 && <p className = "wind">{props.windSpeed4}</p>}
-            {props.overallHumidity4 && <p className = "get-humid">{props.overallHumidity4}</p>}
-            {props.overallDescription4 && <p className = "conditions">Conditions: {props.overallDescription4}</p>}
-        </div>
-        <div className = "fifth-day-container">
-            {props.day5 && <p className = "current-time">{props.day5}</p>}
-            <img className = 'style-condition' src = {props.icon5} alt = 'rain and clouds'></img>
-            {props.overallTemperature5 && <p className = "current-temp">{props.overallTemperature5}</p>}
-            {props.windSpeed5 && <p className = "wind">{props.windSpeed5}</p>}
-            {props.overallHumidity5 && <p className = "get-humid">{props.overallHumidity5}</p>}
-            {props.overallDescription5 && <p className = "conditions">Conditions: {props.overallDescription5}</p>}
-        </div>
-    </div>
-)
+class DayForecast extends React.Component {
+    state = {
+        
+    }
 
-export default FiveDayForecast
+    componentDidMount() {
+        const weather = this.props.weather
+        console.log('hell with', weather)
+        this.getMaxMin(weather)
+        this.setState({
+            weatherType: weather[0].weather[0].main
+        })
+    }
+
+    getMaxMin =(array) => {
+        console.log('days weather', array)
+        let lowest = 100;
+        let highest = 0;
+        let tmp;
+        for (var i=0; i<array.length; i++) {
+            tmp = array[i].main.temp;
+            tmp = tmp * (9 / 5) - 459.67;
+            if (tmp < lowest) lowest = tmp;
+            if (tmp > highest) highest = tmp;
+        }
+        console.log('day high', highest, 'day low', lowest);
+        this.setState({
+            highTemp: highest,
+            lowTemp: lowest
+        })
+
+    }
+
+    getIcon = (x) => {
+        if (x === "clear") {
+            return <img src={sunny} />
+        }
+        if (x === "clear") {
+            return  <img src={sunny} />
+        }
+        if (x === "clear") {
+            return  <img src={sunny} />
+        }
+        if (x === "clear") {
+            return  <img src={sunny} />
+        }
+        else {
+            return  <img src={sunny} />
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                { this.getIcon(this.state.weatherType)}
+            </div>
+        )
+    }
+}
+    
+export default DayForecast
+
+    
+        // <div className = "five-day-container">
+        //  <div className = "first-day-container">
+        //     {props.day1 && <p className = "current-time">{props.day1}</p>}
+        //      <img src = {props.icon} alt = 'conditions'></img>
+        //      {props.currentTemperature && <p className = "current-temp">{props.currentTemperature}</p>}
+        //     {props.windSpeed && <p className = "wind">{props.windSpeed}</p>} 
+        //      {props.overallHumidity && <p className = "get-humid"> {props.overallHumidity}</p>}
+        //      {props.overallDescription && <p className = "conditions">Conditions: {props.overallDescription}</p>} 
+        // </div>
+        //   <div className = "second-day-container">
+        //      {props.day2 && <p className = "current-time">{props.day2}</p>}
+        //     <img className = 'style-condition' src =    {props.icon2} alt = 'conditions'></img>
+        //       {props.overallTemperature2 && <p className = "current-temp">{props.overallTemperature2}</p>}
+        //     {props.windSpeed2 && <p className = "wind">{props.windSpeed2}</p>}
+        //     {props.overallHumidity2 && <p className = "get-humid">{props.overallHumidity2}</p>}
+        //      {props.overallDescription2 && <p className = "conditions">Conditions: {props.overallDescription2}</p>}
+        // </div>
+        //  <div className = "third-day-container">
+        //      {props.day3 && <p className = "current-time">{props.day3}</p>}
+        //      <img className = 'style-condition' src = {props.icon3} alt = 'rain and clouds'></img>
+        //      {props.overallTemperature3 && <p className = "current-temp">{props.overallTemperature3}</p>}
+        //      {props.windSpeed3 && <p className = "wind">{props.windSpeed3}</p>}
+        //      {props.overallHumidity3 && <p className = "get-humid">{props.overallHumidity3}</p>}
+        //     {props.overallDescription3 && <p className = "conditions">Conditions: {props.overallDescription3}</p>}
+        //  </div>
+        // <div className = "fourth-day-container">
+        //     {props.day4 && <p className = "current-time">{props.day4}</p>}
+        //     <img className = 'style-condition' src = {props.icon4} alt = 'rain and clouds'></img>
+        //      {props.overallTemperature4 && <p className = "current-temp">{props.overallTemperature4}</p>}
+        //      {props.windSpeed4 && <p className = "wind">{props.windSpeed4}</p>}
+        //  {props.overallHumidity4 && <p className = "get-humid">{props.overallHumidity4}</p>}
+        //      {props.overallDescription4 && <p className = "conditions">Conditions: {props.overallDescription4}</p>}
+        //  </div>
+        //  <div className = "fifth-day-container">
+        //      {props.day5 && <p className = "current-time">{props.day5}</p>}
+        //      <img className = 'style-condition' src = {props.icon5} alt = 'rain and clouds'></img>
+        //      {props.overallTemperature5 && <p className = "current-temp">{props.overallTemperature5}</p>}
+        //      {props.windSpeed5 && <p className = "wind">{props.windSpeed5}</p>}
+        //      {props.overallHumidity5 && <p className = "get-humid">{props.overallHumidity5}</p>}
+        //      {props.overallDescription5 && <p className = "conditions">Conditions: {props.overallDescription5}</p>} 
+        //  </div> 
+        //  </div> 
+        
+    
+
